47242cb051e4e79601edea058f0f0df818a2bc2b
Gaurav <gauravsoni54670@gmail.com>
2026-04-04 02:59:41 +0530

    commit

--- src/main/java/com/gaurav/RedisDataType/controller/SortedSetController.java
+++ src/main/java/com/gaurav/RedisDataType/controller/SortedSetController.java
@@ -3,10 +3,9 @@ package com.gaurav.RedisDataType.controller;
 import com.gaurav.RedisDataType.service.SortedSetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api/sorted-set")
@@ -21,5 +20,39 @@ public class SortedSetController {
             @RequestParam("score") double score) {
         this.sortedSetService.addMemberWithScore(key,member,score);
         return ResponseEntity.ok(member + " added with score " + score);
+    }
+//    get member's score
+    @GetMapping("/get-score")
+    public ResponseEntity<?> getScore(
+            @RequestParam("key") String key,
+            @RequestParam("member") String member) {
+        Object response = this.sortedSetService.getScore(key, member);
+        return ResponseEntity.ok("Response: " + response);
+    }
+//    get the rank of member
+    @GetMapping("/get-rank")
+    public ResponseEntity<?> getRank(
+            @RequestParam("key") String key,
+            @RequestParam("member") String member) {
+        Long result = this.sortedSetService.getRankOfMember(key, member);
+        return ResponseEntity.ok(
+                result != null? "Member rank is: " + result : "No member found. Rank is null"
+        );
+    }
+//    get all the members in ascending order
+    @GetMapping("/all-acs-members")
+    public ResponseEntity<?> getMembersInAscending(@RequestParam("key") String key) {
+        Set<Object> response = this.sortedSetService.getAllMembersInAscending(key);
+        return ResponseEntity.ok(
+                (response == null || response.isEmpty()) ?
+                        "No member present." : response);
+    }
+    //    get all the members in ascending order
+    @GetMapping("/all-desc-members")
+    public ResponseEntity<?> getMembersInDescending(@RequestParam("key") String key) {
+        Set<Object> response = this.sortedSetService.getAllMembersInDescending(key);
+        return ResponseEntity.ok(
+                (response == null || response.isEmpty()) ?
+                        "No member present." : response);
     }
 }
--- src/main/java/com/gaurav/RedisDataType/service/SortedSetService.java
+++ src/main/java/com/gaurav/RedisDataType/service/SortedSetService.java
@@ -4,13 +4,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+
 @Service
 public class SortedSetService {
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
 //    add member with score
-    public Boolean addMemberWithScore(
+    public void addMemberWithScore(
             String key, String member, double score) {
-        return redisTemplate.opsForZSet().add(key, member, score);
+        redisTemplate.opsForZSet().add(key, member, score);
     }
+//    get member and score
+    public Object getScore(String key, String member) {
+        return redisTemplate.opsForZSet().score(key, member);
+    }
+//    get the rank of member
+    public Long getRankOfMember(String key, String member) {
+        return redisTemplate.opsForZSet().rank(key, member);
+    }
+//    get all member in ascending order
+    public Set<Object> getAllMembersInAscending(String key) {
+        return redisTemplate.opsForZSet().range(key, 0, -1);
+    }
+//    get all member in descending order
+    public Set<Object> getAllMembersInDescending(String key) {
+        return redisTemplate.opsForZSet().reverseRange(key, 0, - 1);
+    }
+
 }
